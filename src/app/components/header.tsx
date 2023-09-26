0579c541ac6f46f3e838464796bcfea8cba151a5
Pedro Jonas <pedrojonas.pj77@gmail.com>
2023-09-26 10:06:36 -0300

    feat: create Header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header () {
+    return(
+        <div className="flex flex-col w-full font-sans text-slate-50 h-20 items-initial bg-amber-400">
+            <h2 className="text-4xl font-bold ps-5 pt-2">YourFood</h2>
+            <h1 className="text-xs ps-6"> Selecionou, pediu, chegou! </h1>
+        </div>
+    )
+};
